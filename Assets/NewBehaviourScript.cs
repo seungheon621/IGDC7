0172c8cbde5e3b16663a50d9ea557c5a6d0c2899
seungheon621 <sddyy69@naver.com>
2021-06-30 23:30:00 +0900

    #Dino Run

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+   public float Level(int score)
+    {
+        int defaultSpeed = -5;
+        int increSpeed = (score * -1) / 100;
+        if (increSpeed < -5)
+            increSpeed = -5;
+
+        return defaultSpeed + increSpeed;
+    }
+}
